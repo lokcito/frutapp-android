b9012f9a5fc406d2f19226cedb9cc141a062d35c
Plaga Enciso <plaga@Plagas-iMac.local>
2018-11-08 16:09:53 -0500

    Sesion-4: Limpiando callbacks

--- src/app/src/main/java/info/rayrojas/bichito/frutapp/MainActivity.java
+++ src/app/src/main/java/info/rayrojas/bichito/frutapp/MainActivity.java
@@ -15,31 +15,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d("Debuging", "La aplicacion entró en start");
     }
     @Override
     public void onResume(){
         super.onResume();
-        Log.d("Debuging", "La aplicacion entró en resume");
-        // put your code here...
 
     }
     @Override
     protected void onStop() {
-        // call the superclass method first
         super.onStop();
-        Log.d("Debuging", "La aplicacion entró en stop");
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.v("Debuging", "La aplicacion entró en pause");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d("Debuging", "La aplicacion entró en destroy");
     }
 }

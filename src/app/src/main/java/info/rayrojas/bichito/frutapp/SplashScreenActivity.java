b5e083722fd4f7a321695922d0161f3d5343f4b5
Plaga Enciso <plaga@Plagas-iMac.local>
2018-11-14 14:58:40 -0500

    Insertando SplashScreen a nuestra aplicacion

---
+++ src/app/src/main/java/info/rayrojas/bichito/frutapp/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package info.rayrojas.bichito.frutapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent o = new Intent(this, MainActivity.class);
+        startActivity(o);
+        finish();
+    }
+}

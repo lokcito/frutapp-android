5af3eae37a6ef51f5fe8ba21017a2885857aa275
Plaga Enciso <plaga@Plagas-iMac.local>
2018-11-14 17:09:36 -0500

    Login con acceso a completar formularios

--- src/app/src/main/java/info/rayrojas/bichito/frutapp/SplashScreenActivity.java
+++ src/app/src/main/java/info/rayrojas/bichito/frutapp/SplashScreenActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import info.rayrojas.bichito.frutapp.activities.LoginActivity;
+
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent o = new Intent(this, MainActivity.class);
+        Intent o = new Intent(this, LoginActivity.class);
         startActivity(o);
         finish();
     }
